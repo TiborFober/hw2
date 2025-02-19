ef89429f57e6c2a1662ef752f132944f45026a6a
Tibor Fober <fober@usc.edu>
2025-02-18 23:31:34 -0800

    worked on MyDataStore class and implemented all the functions

--- mydatastore.cpp
+++ mydatastore.cpp
@@ -1,3 +1,176 @@
 #include "mydatastore.h"
+#include "util.h"
 
 using namespace std;
+
+MyDataStore::~MyDataStore()
+{
+
+}
+
+void MyDataStore::addProduct(Product* p)
+{
+	products_.push_back(p);
+
+	set<string> keywords = p->keywords();
+
+	for (set<string>::iterator it = keywords.begin(); it != keywords.end(); ++it)
+	{
+		keywordMap[*it].insert(p);
+	}
+}
+
+void MyDataStore::addUser(User* u)
+{
+	users_.insert(make_pair(u->getName(), u));
+}
+
+vector<Product*> MyDataStore::search(vector<string>& terms, int type)
+{
+	set<Product*> result;
+
+	if (terms.empty())
+	{
+		return vector<Product*>();
+	}
+
+	for (vector<string>::iterator itTerms = terms.begin(); itTerms != terms.end(); ++itTerms)
+	{
+		map<string, set<Product*>>::iterator itMap = keywordMap.find(*itTerms);
+
+		if (itMap != keywordMap.end())
+		{
+			set<Product*> productSet = itMap->second;
+
+			if (type == 0) // AND (intersection)
+			{
+				if (result.empty())
+				{
+					result = productSet;
+				}
+				else
+				{
+					result = setIntersection(result, productSet);
+				}
+
+
+			}
+			else if(type == 1) // OR (union)
+			{
+				result = setUnion(result, productSet);
+			}
+		}
+		else if (type == 0) // AND search and keyword not found, return empty vector
+		{
+			return vector<Product*>();
+		}
+
+	}
+
+	vector<Product*> returnVal;
+
+	for (set<Product*>::iterator it = result.begin(); it != result.end(); ++it)
+	{
+		returnVal.push_back(*it);
+	}
+
+	return returnVal;
+}
+
+void MyDataStore::dump(ostream& ofile)
+{
+	ofile << "<products>" << endl;
+	for (vector<Product*>::iterator it = products_.begin(); it != products_.end(); ++it)
+	{
+		(*it)->dump(ofile);
+	}
+	ofile << "</products>" << endl;
+
+	ofile << "<users>" << endl;
+	for (map<string, User*>::iterator it = users_.begin(); it != users_.end(); ++it)
+	{
+		it->second->dump(ofile);
+	}
+	ofile << "</users>" << endl;
+
+}
+
+
+void MyDataStore::addToCart(std::string username, Product* p)
+{
+	if (users_.find(username) == users_.end())
+	{
+		cout << "Invalid username" << endl;
+		return;
+	}
+
+	shoppingCart_[username].push(p);
+}
+
+void MyDataStore::viewCart(std::string username)
+{
+	if (users_.find(username) == users_.end())
+	{
+		cout << "Invalid username" << endl;
+		return;
+	}
+
+	if (shoppingCart_.find(username) == shoppingCart_.end() || shoppingCart_[username].empty())
+	{
+		cout << "Cart is empty or username not found" << endl;
+		return; 
+	}
+
+	queue<Product*> tempCart = shoppingCart_[username];
+
+	int index = 1;
+
+	while (!tempCart.empty())
+	{
+		Product* prod = tempCart.front();
+		cout << "Item " << index << ":" << endl;
+		cout << prod->displayString() << endl;
+		cout << endl;
+
+		tempCart.pop();
+		index++;
+
+	}
+
+}
+
+void MyDataStore::buyCart(std::string username)
+{
+	if (users_.find(username) == users_.end())
+	{
+		cout << "Invalid username" << endl;
+		return;
+	}
+
+	if (shoppingCart_.find(username) == shoppingCart_.end())
+	{
+		cout << "No shopping cart for user" << endl;
+		return;
+	}
+
+	queue<Product*>& tempCart = shoppingCart_[username];
+	queue<Product*> newCart;
+
+	while (!tempCart.empty())
+	{
+		Product* prod = tempCart.front();
+		tempCart.pop();
+
+		if (users_[username]->getBalance() >= prod->getPrice()
+			&& prod->getQty() > 0)
+		{
+			users_[username]->deductAmount(prod->getPrice());
+			prod->subtractQty(1);
+		}
+		else
+		{
+			newCart.push(prod);
+		}
+	}
+	shoppingCart_[username] = newCart;
+}
--- mydatastore.h
+++ mydatastore.h
@@ -2,7 +2,30 @@
 #define MYDATASTORE_H
 
 #include "datastore.h"
+#include <map>
+#include <queue>
 
+class MyDataStore : public DataStore
+{
+public:
+	~MyDataStore();
+
+	virtual void addProduct(Product* p);
+	virtual void addUser(User* u);
+	virtual std::vector<Product*> search(std::vector<std::string>& terms, int type);
+	virtual void dump(std::ostream& ofile);
+
+	// Shopping cart functions
+	void addToCart(std::string username, Product* p);
+	void viewCart(std::string username);
+	void buyCart(std::string username);
+
+private:
+	std::vector<Product*> products_;
+	std::map<std::string, User*> users_;
+	std::map<std::string, std::set<Product*>> keywordMap;
+	std::map<std::string, std::queue<Product*>> shoppingCart_;
+};
 
 
 #endif
