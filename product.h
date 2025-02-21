23cadd1419d0f92148bb2020c5e3dd88fdfde090
Tibor Fober <fober@usc.edu>
2025-02-20 19:32:20 -0800

    changed typename in util.h

--- product.h
+++ product.h
@@ -48,8 +48,4 @@ protected:
 };
 
 
-
-
-
-
 #endif
--- util.h
+++ util.h
@@ -15,7 +15,7 @@ std::set<T> setIntersection(std::set<T>& s1, std::set<T>& s2)
 {
 	std::set<T> intersectionSet;
 
-	for (std::set<T>::iterator it = s1.begin(); it != s1.end(); ++it)
+	for (typename std::set<T>::iterator it = s1.begin(); it != s1.end(); ++it)
 	{
 		if (s2.find(*it) != s2.end()) // element found, add to intersectionSet
 		{
