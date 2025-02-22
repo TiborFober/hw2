820f34076991f5c2d72018eedad4a401838d6afb
Tibor Fober <fober@usc.edu>
2025-02-21 19:08:54 -0800

    changes

--- clothing.cpp
+++ clothing.cpp
@@ -7,25 +7,14 @@ using namespace std;
 // Clothing
 std::set<std::string> Clothing::keywords() const 
 {
-	/*
 	std::set<std::string> keySet;
 	std::set<std::string> nameWords = parseStringToWords(name_);
 	std::set<std::string> brandWords = parseStringToWords(brand_);
 
 	keySet = setUnion(nameWords, brandWords);
 
-	return keySet; */
-	std::set<std::string> keychain = parseStringToWords(name_);
+	return keySet;
 
-	set<string> brandSet = parseStringToWords(brand_);
-	set<string>::iterator it;
-
-	for (it = brandSet.begin(); it != brandSet.end(); it++) {
-		keychain.insert(*it);
-	}
-	keychain.insert(convToLower(size_));
-
-	return keychain;
 }
 
 
@@ -41,9 +30,6 @@ string Clothing::displayString() const
 
 void Clothing::dump(std::ostream& os) const
 {
-	os << category_ << "\n" << name_ << "\n" << price_ << "\n" << qty_ <<
-		"\n" << size_ << "\n" << brand_ << std::endl;
-
-	//Product::dump(os);
-	//os << size_ << "\n" << brand_ << endl;
+	Product::dump(os);
+	os << size_ << "\n" << brand_ << endl;
 }
